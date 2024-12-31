e4484201f804af05025e0d6f0394a3e209f5d64a
Javier <somiryu@gmail.com>
2024-12-31 07:30:46 -0500

    changes

---
+++ src/lib/Game.svelte
@@ -0,0 +1,37 @@
+<script>
+  import db from "./db";
+  export let cat = "nouns"
+  let rolled = false
+
+  function getRandomElement(array) {
+    const randomIndex = Math.floor(Math.random() * array.length); // Generate random index
+    return array[randomIndex]; // Return the element at the random index
+}
+
+  const roll = () => {
+    rolled = getRandomElement(db[cat])
+  }
+</script>
+
+<controls-group>
+  <button on:click={()=> cat = "home"}>Atrás</button>
+  {#if rolled}
+    <button on:click={roll}>Relanzar</button>
+  {/if}
+</controls-group>
+{#if !rolled}
+  Vas a recibir una palabra a frase al oprimir el botón
+  <button on:click={roll}>*** Roll ***</button>
+{:else}
+  <h1 on:click={roll}>{rolled}</h1>
+{/if}
+
+
+
+<style>
+  controls-group{display: flex; gap: 1rem; position: fixed; top: 1rem; left: 1rem}
+  controls-group > button{width: auto;padding: .5rem}
+  h1::first-letter {
+    text-transform: uppercase;  
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Game from "$lib/Game.svelte";
+
+  let gameView = "home"
+</script>
+
+
+<playground-area>
+  {#if gameView === "home"}
+    <h1>Escoge un juego</h1>
+    <p>Te lanzaremos una palabra o frase aleatoria</p>
+    <button on:click={()=> gameView = "nouns"}>Sustantivos</button>
+    <button on:click={()=> gameView = "stats"}>Estadístico</button>
+  {:else}
+    <Game bind:cat={gameView}/>
+  {/if}
+</playground-area>
+
+<style>
+  :global(html){background-color: black; color: white}
+  playground-area{
+    height: 100dvh; width: 100vw; padding: 2rem;
+    display: flex; text-align: center; justify-content: center;
+    flex-direction: column;
+    box-sizing: border-box;
+  }
+
+  :global(button){
+    background-color: black; color: white; border: 2px solid white;
+    cursor: pointer; transition: .5s; width: 30rem; margin: 1rem auto; padding: 2rem; 
+    font-size: 1rem;
+  }
+
+  :global(button):hover{scale: 1.1}
+</style>
